22d0f7deddf22b80df6fbb43ea36f12dde90b555
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-11-08 11:00:15 +0100

    add Utente.java

---
+++ src/main/java/castaldosamuele/progetto_java_settimana7/entities/Role.java
@@ -0,0 +1,5 @@
+package castaldosamuele.progetto_java_settimana7.entities;
+
+public enum Role {
+    USER, ADMIN, ORGANIZER
+}
---
+++ src/main/java/castaldosamuele/progetto_java_settimana7/entities/Utente.java
@@ -0,0 +1,94 @@
+package castaldosamuele.progetto_java_settimana7.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name =  "utente")
+public class Utente {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public Utente() {}
+
+    public Utente(String username, String nome, String cognome, String email, String password) {
+        this.username = username;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.password = password;
+        this.role = Role.USER;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
