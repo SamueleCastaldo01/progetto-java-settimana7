df45644f29763efec95db1b0650b06d670b9c07c
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-11-08 12:18:39 +0100

    ho aggiunto la classe utente service adesso vado ad gestire i vari controlli sia per il login che per la registrazione e andiamo a vedere se funzionano

--- src/main/java/castaldosamuele/progetto_java_settimana7/services/UtenteService.java
+++ src/main/java/castaldosamuele/progetto_java_settimana7/services/UtenteService.java
@@ -1,11 +1,15 @@
 package castaldosamuele.progetto_java_settimana7.services;
 
+import castaldosamuele.progetto_java_settimana7.Exceptions.BadRequestException;
 import castaldosamuele.progetto_java_settimana7.Exceptions.NotFoundException;
+import castaldosamuele.progetto_java_settimana7.Payloads.NewUtenteDTO;
 import castaldosamuele.progetto_java_settimana7.Repositories.UtenteRepository;
 import castaldosamuele.progetto_java_settimana7.entities.Utente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UtenteService {
@@ -23,4 +27,51 @@ public class UtenteService {
         return this.utenteRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("L'utente con email " + email + " non è stato trovato"));
     }
 
+    //GET -------------------------------------
+    public List<Utente> findAll() {
+        return this.utenteRepository.findAll();
+    }
+
+    //POST -------------------------------------
+    public Utente save(NewUtenteDTO body) {
+        //controllo se ci sta già un altro con la stessa email
+        this.utenteRepository.findByEmail(body.email()).ifPresent(
+                user -> {
+                    throw new BadRequestException("Email " + body.email() + " già in uso!");
+                }
+        );
+        //controllo anche dell'username, deve essere univoco
+        this.utenteRepository.findByUsername(body.username()).ifPresent(
+                user -> {
+                    throw new BadRequestException("Username " + body.username() + " è già in uso!");
+                }
+        );
+        Utente newUtente = new Utente(body.username(), body.nome(), body.cognome(), body.email(), bcrypt.encode(body.password()));
+        return this.utenteRepository.save(newUtente);
+    }
+
+    //PUT --------------------------------------------
+    public Utente findByIdAndUpdate(long id, NewUtenteDTO body) {
+        Utente found = this.findById(id);
+
+        if (!found.getEmail().equals(body.email())) {
+            this.utenteRepository.findByEmail(body.email()).ifPresent(
+                    user -> {
+                        throw new BadRequestException("Email " + body.email() + " già in uso!");
+                    }
+            );
+        }
+        found.setUsername(body.username());
+        found.setNome(body.nome());
+        found.setCognome(body.cognome());
+        found.setEmail(body.email());
+
+        return this.utenteRepository.save(found);
+    }
+
+    //DELETE --------------------------------------------
+    public void findByIdAndDelete(long id) {
+        Utente found = this.findById(id);
+        this.utenteRepository.delete(found);
+    }
 }
