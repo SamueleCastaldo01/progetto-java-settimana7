1336dfc777acbf32b69808f0ee56aea4a3e61903
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-11-08 10:00:46 +0100

    Initial commit

---
+++ src/main/java/castaldosamuele/progetto_java_settimana7/ProgettoJavaSettimana7Application.java
@@ -0,0 +1,13 @@
+package castaldosamuele.progetto_java_settimana7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoJavaSettimana7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoJavaSettimana7Application.class, args);
+	}
+
+}
---
+++ src/test/java/castaldosamuele/progetto_java_settimana7/ProgettoJavaSettimana7ApplicationTests.java
@@ -0,0 +1,13 @@
+package castaldosamuele.progetto_java_settimana7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoJavaSettimana7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
